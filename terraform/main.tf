e8390cf9d0a9ef4b69b1f1d79935e00b3fc1fc66
Kehindeomotayo <kennyjay_2@yahoo.com>
2025-08-10 13:36:13 +0100

    Remove all module references from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,35 +3,3 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = var.common_tags
 }
-
-module "vnet" {
-  source              = "./modules/vnet"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  resource_prefix     = var.resource_prefix
-  common_tags         = var.common_tags
-}
-
-module "vm" {
-  source              = "./modules/vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  resource_prefix     = var.resource_prefix
-  common_tags         = var.common_tags
-}
-
-module "storage" {
-  source              = "./modules/storage"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  resource_prefix     = var.resource_prefix
-  common_tags         = var.common_tags
-}
-
-module "policy" {
-  source              = "./modules/policy"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  resource_prefix     = var.resource_prefix
-  common_tags         = var.common_tags
-}
