ce54a485ba05f149a44bc6fe8e436cbcfff79e7a
Kehindeomotayo <kennyjay_2@yahoo.com>
2025-08-10 15:14:41 +0100

    updated codes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,20 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "azurerm" {
-  # These will automatically use ARM_* environment variables
-  # No need to explicitly set them
   features {}
+  
+  # Explicitly disable other authentication methods
+  use_cli = false
+  use_msi = false
+  
+  # The ARM_* environment variables will be automatically used
+  # No need to explicitly set subscription_id, tenant_id, etc.
 }
-#
